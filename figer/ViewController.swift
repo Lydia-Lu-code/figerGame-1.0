8b24c113f577b5bc900aa5a103b0002789bf67c4
Lydia-Lu-code <guipopo5@gmail.com>
2020-08-21 22:39:01 +0800

    Initial Commit

---
+++ figer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  figer
+//
+//  Created by 維衣 on 2020/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
